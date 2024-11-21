94808478f314c587f28ae1b57b6dce823087e96e
PhoenixBrehm <phoenixbrehm2700@gmail.com>
2024-11-21 16:35:27 -0600

    Added TODO comment
    Added todo comment and the description of the process of how the parser will perform these processes

--- ProjectFiles/parser.cpp
+++ ProjectFiles/parser.cpp
@@ -6,3 +6,7 @@ Purpose: To take in an input from the lexer in the form of a linked list and out
 
 By post order the intention is to go as far down as possible, then once you have two numbers go up go to the common parent and perform the operation, continue until the root is missing.
 */
+//Assuming clean input in the form of a linked list
+//TODO: Throw error to errorhandler when we find the doubling up of operators, except for cases of nested parenthesis (()), distibution ()(), ()Operator()
+//Checking parenthesis validity
+//Look through the linked list from right to left, finding the lowest precidence operator [ +-, */and distributive, ^, () ] then putting the operator at the root, and then the leftside of the expression on the left child and the right side of the expression into the right child, perform this operation until every item of the list is isolated, then return this new tree to the evaluator
