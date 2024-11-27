d36bd1dc8b3f0c4d2e8da449f80064088e5c9c0b
PhoenixBrehm <phoenixbrehm2700@gmail.com>
2024-11-27 16:20:13 -0600

    Updated Linked list
    The linked list still needs its removal, and destructor implemented. But currently I believe this works.
    getEntry is incomplete as it currently only prints characters rather than returning characters.

--- ProjectFiles/dataStruct.cpp
+++ ProjectFiles/dataStruct.cpp
@@ -1,26 +1,44 @@
 #include <stdexcept>
 #include <iostream>
+#include <cmath>
 using namespace std;
-template <typename T>
-struct Node{
-    T entry;
-    Node *next; //next points to the address of another node
+
+union nodeData{
+    char character;
+    double value;
 };
 
-template <typename T>
+struct Node{
+    nodeData entry;
+    Node *next; //next points to the address of another node
+    bool isChar; //used with the getValue method of linkedList, as well as could be useful to use ptr.isChar to determine what to do.
+
+    //Our constructor cases, if character or if value
+    Node(char c){
+        entry.character = c;
+        isChar = true;
+        next = nullptr;
+    }
+    Node(double v){
+        entry.value = v;
+        isChar = false;
+        next = nullptr;
+    }
+};
+
 class LinkedList{
     private:
-    Node *head;
+    Node *head; //head will point to the address of a node
     int length;
     public:
-    LinkedList(){
+    LinkedList(){ //constructor
         head = nullptr;
         length =0;
     }
-    int length(){
+    int getLength(){
         return length;
     }
-    T getEntry(int index){
+    double getEntry(int index){
         if(length == 0){
             cout << "List is empty\n";
         }else if(index >= length || index < 0){
@@ -28,24 +46,104 @@ class LinkedList{
         }
         Node *temp = head;
         for(int i =0;i<index;i++){
-            temp = temp->next;
+            temp = temp->next; //temp will point at the next node in line
         }
-        return temp->entry;
-    }
-    void insert(T value,int index){
-        if(length == 0){
-            Node *insertVal = new Node();
-            insertVal->entry = value;
-            insertVal->next = nullptr;
-            head = insertVal;
-        }else if(index >= length || index < 0){
-            throw out_of_range("Index out of bounds");
+        if(temp->isChar){
+            cout << temp->entry.character << "\n";
         }else{
-            //Impliment
+            return temp->entry.value;
+        }
+    }
+
+    void insert(char c,int index){ //USED FOR Characters
+        Node *jumper = head;
+        Node *insertVal = new Node(c);
+        insertVal->next = nullptr;
+        if(index > length || index < 0){
+            throw out_of_range("Index out of bounds");
+
+
+        }else if(length == 0){ //if we are putting in our first node
+            head = insertVal;
+            length = 1;
+
+        }else if(length > 0 && index != 0){//executed when going for an item not at the front
+            Node *prejump = head;
+            for(int i=0;i<index;i++){
+                if(i>0){
+                    prejump = prejump->next;
+                }
+                jumper = jumper->next;
+            }
+            insertVal->next = jumper; //insertval is now pointing at the node ahead of it
+            prejump->next = insertVal; //prejump is now the node behind insertVal
+            length = length + 1;
+
+        }else{//executed when we have items in the list, but are inserting at the beginning
+            head = insertVal;
+            head->next = jumper;
+            length = length + 1;
+        }
+    }
+
+    void insert(double v,int index){ //USED for values
+        Node *jumper = head;
+        Node *insertVal = new Node(v);
+        insertVal->next = nullptr;
+        if(index > length || index < 0){
+            throw out_of_range("Index out of bounds");
+
+
+        }else if(length == 0){ //if we are putting in our first node
+            head = insertVal;
+            length = 1;
+
+        }else if(length > 0 && index != 0){//executed when going for an item not at the front
+            Node *prejump = head;
+            for(int i=0;i<index;i++){
+                if(i>0){
+                    prejump = prejump->next;
+                }
+                jumper = jumper->next;
+            }
+            insertVal->next = jumper; //insertval is now pointing at the node ahead of it
+            prejump->next = insertVal; //prejump is now the node behind insertVal
+            length = length + 1;
+
+        }else{//executed when we have items in the list, but are inserting at the beginning
+            head = insertVal;
+            head->next = jumper;
+            length = length + 1;
         }
     }
 };
 
+
+/* SAMPLE MAIN
+
+
+
+int main(){
+    LinkedList list;
+    list.insert(4.0, 0);
+    list.insert(6.0, 0);
+    list.insert(3.0, 1);
+    list.insert(8.0, 2);
+    list.insert('+', 2);
+
+    for(int i=0;i<list.getLength();i++){
+        if(!isnan(list.getEntry(i))){                       <-This line is used because of the return method for getEntry not being the most well done, if anybody does a better implementation that would be nice
+            cout << list.getEntry(i) << "\n";
+        }
+    }
+    while (true)
+    {
+        ;
+    }
+    
+    return 0;
+}
+*/
 
 template <typename T>
 struct BinaryNode{
