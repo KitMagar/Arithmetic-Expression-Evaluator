5b5e6d6e6763f8c2fad72159b43488d74d10a697
PhoenixBrehm <phoenixbrehm2700@gmail.com>
2024-12-05 16:16:33 -0600

    Update main.cpp

--- ProjectFiles/main.cpp
+++ ProjectFiles/main.cpp
@@ -9,15 +9,19 @@ int main(){
     bool quit = false;
     string userInput;
     cout << "Welcome to the arithmetic Evaluator\n";
+    cout << "When you're ready to quit type \"QUIT\"\n";
     while(!quit){
         cout << "What is your input: \n";
         cin >> userInput;
+        if(userInput == "QUIT"){
+            break;
+        }
         Lexer lex(userInput);
         Parser parsed(lex.tokenize());
         Evaluator evaluated(parsed);
         cout << evaluated.evaluate();
-        ~lex();
-        ~parsed();
-        ~evaluated();
+        lex.~Lexer();
+        parsed.~Parser();
+        evaluated.~Evaluator();
     }
 }
