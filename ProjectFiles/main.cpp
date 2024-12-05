22b34c2a875561c9ad37ecd46ad9ca80e892f57c
PhoenixBrehm <phoenixbrehm2700@gmail.com>
2024-12-05 15:54:58 -0600

    Create main.cpp

---
+++ ProjectFiles/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "lexer.h"
+#include "parser.h"
+#include "evaluator.h"
+using namespace std;
+
+int main(){
+    bool quit = false;
+    string userInput;
+    cout << "Welcome to the arithmetic Evaluator\n";
+    while(!quit){
+        cout << "What is your input: \n";
+        cin >> userInput;
+        
+    }
+}
