049264fde8745650f3fb034ee3fd6333b5defcc4
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2024-12-05 00:36:08 -0600

    Added sum comments to  evaluator.cpp

---
+++ ProjectFiles/evaluator.cpp
@@ -0,0 +1,14 @@
+// Aiden's Task
+// Created on Dec 5 @ 1230am
+// 
+// Objective : Traverse the expression tree built by parser & evaluate it to produce the final result
+// 
+// Tasks :
+// • Create an EVALUATOR CLASS to perform RECURSIVE traversal of the expression tree, handling 
+// operations based on the node type (operator, operand)
+// • Implement support for basic operators (+,-,*,/,%,**) and ensure proper handling of operator
+// precedence
+// 
+// ALSO :
+// • Handle division by 0 errors and other invalid expressions
+
