2592dea6da9e74c3bb3cc082603e5e3d8755a0af
PhoenixBrehm <phoenixbrehm2700@gmail.com>
2024-12-05 19:53:26 -0600

    Added some debug lines in main

--- ProjectFiles/main.cpp
+++ ProjectFiles/main.cpp
@@ -19,10 +19,37 @@ int main(){
         if(userInput == "QUIT"){
             break;
         }
-        
+        string waitVar;
         Lexer lex(userInput);
+        cout << "Lex";
+        cin >> waitVar;
+        cout << "Input accepted";
+        LinkedList l = lex.tokenize();
+
+            //DEBUG SECTION
+
+                        for(int i=0;i<l.getLength();i++){ //This will be used for debugging
+
+
+                            //This is how we handle if linked list has characters or numbers
+                            if(l.getEntry(i).isChar){ 
+                                cout << l.getEntry(i).entry.character << "C\n";
+                            }else{
+                                cout << l.getEntry(i).entry.value << "V\n";
+                            }
+                        }
+            //IF WE MAKE IT OUT OF THE LOOP TOKENIZER WORKS
+
+
+
         Parser parsed(lex.tokenize());
+        cout << "Parse";
+        cin >> waitVar;
+        cout << "Input accepted";
         Evaluator evaluated(parsed);
+        cout << "Eval";
+        cin >> waitVar;
+        cout << "Input accepted";
         cout << evaluated.evaluate();
         // 💣🔥💥
         lex.~Lexer();
