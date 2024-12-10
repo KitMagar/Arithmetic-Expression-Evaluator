7a4df34189e066d2116293a85dbfaefbc26c769e
Khang-Phan1 <143846350+Khang-Phan1@users.noreply.github.com>
2024-12-10 16:46:03 -0600

    Updated lexer.cpp
    Added support for unary operators, sample inputs 11-15 should work.

--- ProjectFiles/lexer.cpp
+++ ProjectFiles/lexer.cpp
@@ -8,6 +8,7 @@ using namespace std;
 Lexer::Lexer(const string& input) : input(input), pos(0) {}
 LinkedList Lexer::tokenize() {
     LinkedList tokens;
+    double neg = -1;
 
     while (pos < input.length()) {
         char current = input[pos];
@@ -29,12 +30,50 @@ LinkedList Lexer::tokenize() {
             pos++;
         }
         else if (current == '+') {
-            tokens.insert('+', tokens.getLength());
-            pos++;
+            if (pos == 0) {
+                pos++;
+                continue;
+            }
+            else {
+                if (input[pos-1] == '(') {
+                    pos++;
+                    continue;
+                }
+                else {
+                    tokens.insert('+', tokens.getLength());
+                    pos++;
+                }
+            }
         }
         else if (current == '-') {
-            tokens.insert('-', tokens.getLength());
-            pos++;
+            if (pos == 0) {
+                if (pos+1 < input.length() && isdigit(input[pos+1])) {
+                    pos++;
+                    string constant = parseConstant();
+                    tokens.insert(stod(constant)*(-1), tokens.getLength());
+                }
+                else if (pos+1 < input.length() && input[pos+1] == '(') {
+                    tokens.insert(neg, tokens.getLength());
+                    tokens.insert('*', tokens.getLength());
+                    pos++;
+                }
+            }
+            else {
+                if (pos+1 < input.length() && isdigit(input[pos+1]) && input[pos-1] == '(' && input[pos+1] != '(') {
+                    pos++;
+                    string constant = parseConstant();
+                    tokens.insert(stod(constant)*(-1), tokens.getLength());
+                }
+                else if (pos+1 < input.length() && input[pos-1] == '(' && input[pos+1] == '(') {
+                    tokens.insert(neg, tokens.getLength());
+                    tokens.insert('*', tokens.getLength());
+                    pos++;
+                }
+                else {
+                    tokens.insert('-', tokens.getLength());
+                    pos++;
+                }
+            }
         }
         else if (current == '*') {
             if (pos+1 < input.length() && input[pos+1] == '*') {
